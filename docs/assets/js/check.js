0d864f92164029729fe6409cac099a735a8ec30c
simminjeong <pooh5045@naver.com>
2023-05-31 20:38:59 +0900

    complete

--- docs/assets/js/check.js
+++ docs/assets/js/check.js
@@ -47,35 +47,32 @@ function nameCheck(event) {
     }
 }
 
-function birthCheck(event) {
+function birthCheck(button) {
 
-    if (event.key == 'Enter' || event) {
+    const date = new Date();
+    const currentYear = date.getFullYear();// 상수
 
-        const date = new Date();
-        const currentYear = date.getFullYear();// 상수
+    var birthStr = document.querySelector('#birthchk').value;
 
-        var birthStr = document.querySelector('#birthchk').value;
-        var button = document.querySelector('#birthcheckbtn');
-
-        if (birthStr) {
-            if (birthStr.length == 8) {
-                birthYear = parseInt(birthStr.substring(0, 4));
-                let age = currentYear - birthYear + 1;
-                if (age >= 120) {
-                    alert("생년월일 제대로 입력했는지 확인하세요")
-                } else {
-                    alert(`현재 ${age}세 입니다`)
-                    button.innerText = '확인완료';
-                    button.style.backgroundColor = 'green';
-                }
+    if (birthStr) {
+        if (birthStr.length == 8) {
+            birthYear = parseInt(birthStr.substring(0, 4));
+            let age = currentYear - birthYear + 1;
+            if (age >= 120) {
+                alert("생년월일 제대로 입력했는지 확인하세요")
+            } else {
+                alert(`현재 ${age}세 입니다`)
+                button.innerText = '확인완료';
+                button.style.backgroundColor = 'green';
             }
-            else {
-                alert("19981223 형식으로 작성해주세요");
-            }
-        } else {
-            alert("아무것도 입력하지 않았습니다.")
         }
+        else {
+            alert("19981223 형식으로 작성해주세요");
+        }
+    } else {
+        alert("아무것도 입력하지 않았습니다.")
     }
+
 }
 
 function addressCheck(event) {
@@ -96,26 +93,23 @@ function addressCheck(event) {
     }
 }
 
-function phonenumberCheck(event) {
+function phonenumberCheck(button) {
 
-    if (event.key == 'Enter' || event) {
+    var phonenumber = document.querySelector('#phonenumberchk').value;
 
-        var phonenumber = document.querySelector('#phonenumberchk').value;
-        var button = document.querySelector('#phonecheckbtn');
-
-        if (phonenumber) {
-            if (phonenumber.length == 11) {
-                alert(`${phonenumber} 전화번호 등록`)
-                button.innerText = '확인완료';
-                button.style.backgroundColor = 'green';
-            } else {
-                alert("01012345678 형식으로 작성해주세요")
-            }
-        }
-        else {
-            alert("아무것도 입력하지 않았습니다.");
+    if (phonenumber) {
+        if (phonenumber.length == 11) {
+            alert(`${phonenumber} 전화번호 등록`)
+            button.innerText = '확인완료';
+            button.style.backgroundColor = 'green';
+        } else {
+            alert("01012345678 형식으로 작성해주세요")
         }
     }
+    else {
+        alert("아무것도 입력하지 않았습니다.");
+    }
+
 }
 
 function notcomplete(button) {
